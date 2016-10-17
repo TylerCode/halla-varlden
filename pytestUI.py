b025620fb467b1e9b3e252ffb69d9d4c7432267e
Tyler C <tyler.christenson@live.com>
2016-10-17 13:22:26 -0600

    Some additional shenanigans
    Added a quit button, changed the color back to normal cause I have no
    idea how to use themes (GG RIP)

--- pytestUI.py
+++ pytestUI.py
@@ -1,23 +1,30 @@
 # A small test/example of making a user interface with Python. Useful especially for 
 # the raspberry pi. (When you have half a dozen of the things, you never get bored)
 
-from Tkinter import Tk, Frame, BOTH
+from Tkinter import Tk, BOTH
+from ttk import Frame, Button, Style
 
 class Taco(Frame):
   
     def __init__(self, parent):
-        Frame.__init__(self, parent, background="white")   
+        Frame.__init__(self, parent)   
          
         self.parent = parent
         
         self.initUI()
         
-    
+        
     def initUI(self):
       
-        self.parent.title("Taco")
+        self.parent.title("Tacos for days")
+        self.style = Style()
+        self.style.theme_use("default")
+
         self.pack(fill=BOTH, expand=1)
 
+        quitButton = Button(self, text="Quit",
+            command=self.quit)
+        quitButton.place(x=5, y=5)
 
 
 def main():
@@ -29,4 +36,4 @@ def main():
 
 
 if __name__ == '__main__':
-    main()
+    main()  
