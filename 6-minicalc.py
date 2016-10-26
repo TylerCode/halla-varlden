42e17b7e05c18734ce1f017fafd3d504127d5c4a
Tyler C <tyler.christenson@live.com>
2016-10-25 19:05:39 -0600

    added to minicalc
    Probably will toss this program

--- 6-minicalc.py
+++ 6-minicalc.py
@@ -5,6 +5,12 @@
 print "This will do very basic math. Input your problem like this <Number> <Operator> <Number>"
 inputString = str(raw_input("input: ")) #The input string
 
+tryparse(inputString)
+
+# horrible program, sorry guys, don't look
+# lets just sweep this under the rug
+
+
 # the functions for operations
 
 def add (num1,num2)
@@ -22,5 +28,16 @@ def divide (num1,num2)
 # the other functions
 
 def tryParse (s)
-	if(inputString.find("+") -not -1)
-		#Do stuff
+	parts = inputString.split(" ")
+	if(parts[1].find("+") -not -1)
+		add(parts[0],parts[2])
+		return
+	if(parts[1].find("-") -not -1)
+		subtract(parts[0],parts[2])
+		return
+	if(parts[1].find("*") -not -1 -or parts[1].find("x") -not -1)
+		multiply(parts[0],parts[2])
+		return
+	if(parts[1].find("/") -not -1)
+		divide(parts[0],parts[2])
+		return
