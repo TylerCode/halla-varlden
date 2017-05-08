d416bc1c04e62cfca78c5809960174147c7ae533
TylerCode <tyler.christenson@live.com>
2017-05-08 09:55:28 -0600

    Started testing

--- GameOfLife.py
+++ GameOfLife.py
@@ -1,1 +1,10 @@
-#Will run the famous "Conway's Game of Life"
+# Will run the famous "Conway's Game of Life"
+# Tested under Python 2.7.9
+
+import numpy as numpy
+import matplotlib.pyplot as pyplot
+import matplotlib.animation as animation
+
+x = numpy.array([[0,0,255], [255,255,0], [0,255,0]])
+pyplot.imshow(x, interpolation='nearest')
+pyplot.show()
