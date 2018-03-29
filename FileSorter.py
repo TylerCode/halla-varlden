da5e8f8ac7ba0f55cfadf1a6872262fa6f52137a
Tyler C <TylerCode@users.noreply.github.com>
2018-03-29 11:19:16 -0600

    Delete FileSorter.py

--- FileSorter.py
+++
@@ -1,5 +0,0 @@
-#This program, was originally written to help me move media files into their own folders with related media.
-#If I polish it more, I'll put it into it's own repo and post it around.
-
-#Released under the WTFPL
-
